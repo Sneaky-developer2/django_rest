151287380461046c1465b92fa930b31b5aecdb73
jalaljamalXD <noob123noob123jjxd@gmail.com>
2022-08-28 12:23:18 -0700

    starting project

--- foodOnline_main/urls.py
+++ foodOnline_main/urls.py
@@ -1,4 +1,4 @@
-"""rootProject URL Configuration
+"""foodOnline_main URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
