62b40610630c13d62db8245c4399f452e2f7c2fc
jalaljamalXD <noob123noob123jjxd@gmail.com>
2022-09-13 22:42:43 -0700

    ajax

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,8 +1,10 @@
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, get_object_or_404
 
 from vendor.models import Vendor
 from menu.models import Category, FoodItem
 from django.db.models import Prefetch
+from .models import Cart
 
 
 # Create your views here.
@@ -21,9 +23,36 @@ def vendor_detail(request, vendor_slug):
     categories = Category.objects.filter(vendor=vendor).prefetch_related(
         Prefetch(
             'fooditems',
-            queryset = FoodItem.objects.filter(is_available=True),
+            queryset=FoodItem.objects.filter(is_available=True),
 
         )
     )
     context = {'vendor': vendor, 'categories': categories, }
     return render(request, 'marketplace/vendor_detail.html', context)
+
+
+def add_to_cart(request, food_id):
+    if request.user.is_authenticated:
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            # Check if the food item exists
+            try:
+                fooditem = FoodItem.objects.get(id=food_id)
+                # Check if the user has already added the food to the cart
+                try:
+                    chkCart = Cart.objects.get(
+                        user=request.user, fooditem=fooditem)
+                    # increase the cart quantity
+                    chkCart.quantity += 1
+                    chkCart.save()
+                    return JsonResponse({'status': 'Success', 'message': 'Increased the card!'})
+
+                except:
+                    chkCart = Cart.objects.create(
+                        user=request.user, fooditem=fooditem, quantity=1)
+                    return JsonResponse({'status': 'Success', 'message': 'Added The Food Card'})
+            except:
+                return JsonResponse({'status': 'Failed', 'message': 'This Food does not exist!'})
+        else:
+            return JsonResponse({'status': 'Failed', 'message': 'invalid request!'})
+    else:
+        return JsonResponse({'status': 'Failed', 'message': 'Please login first!'})
