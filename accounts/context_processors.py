a5c693c6f0fc3b85726a47eda8b21985c6e2b2fe
jalaljamalXD <noob123noob123jjxd@gmail.com>
2022-09-25 12:47:48 -0700

    customer profile

--- accounts/context_processors.py
+++ accounts/context_processors.py
@@ -1,3 +1,4 @@
+from accounts.models import UserProfile
 from vendor.models import Vendor
 
 
@@ -6,5 +7,12 @@ def get_vendor(request):
         vendor = Vendor.objects.get(user=request.user)
     except:
         vendor = None
-
     return dict(vendor=vendor)
+
+def get_user_profile(request):
+    try:
+        user_profile = UserProfile.objects.get(user=request.user)
+    except:
+        user_profile = None
+
+    return dict(user_profile=user_profile)
--- customers/views.py
+++ customers/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 from accounts.forms import UserInfoForm, UserProfileForm
 from accounts.models import User, UserProfile
@@ -10,8 +11,25 @@ from accounts.models import User, UserProfile
 @login_required(login_url='login')
 def cprofile(request):
     profile = get_object_or_404(UserProfile, user=request.user)
-    profile_form = UserProfileForm(instance=profile)
-    user_form = UserInfoForm(instance=request.user)
+    if request.method == 'POST':
+        profile_form = UserProfileForm(request.POST, request.FILES, instance=profile)
+        user_form = UserInfoForm(request.POST, instance=request.user)
+        if profile_form.is_valid() and user_form.is_valid():
+            profile_form.save()
+            user_form.save()
+            messages.success(request, 'Profile Updated')
+            return redirect('cprofile')
+        else:
+            print(profile_form.errors)
+            print(user_form.errors)
+            
+    else:
+        profile_form = UserProfileForm(instance=profile)
+        user_form = UserInfoForm(instance=request.user)
 
-    context = {'profile_form': profile_form, 'user_form': user_form}
+    context = {
+        'profile_form': profile_form,
+        'user_form': user_form,
+        'profile': profile,
+    }
     return render(request, 'customers/cprofile.html', context)
